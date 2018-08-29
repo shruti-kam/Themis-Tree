977437754629ad5b5c1edb86d71cf49940b1edd4
Shruti-kam <shruti.kamboj@gmail.com>
2018-08-29 08:43:43 +0530

    Form control changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
--- src/app/stores/stores.component.ts
+++ src/app/stores/stores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder} from '@angular/forms';
 
 import { StoresDataService } from '../services/stores-data.service';
 
@@ -9,18 +10,29 @@ import { StoresDataService } from '../services/stores-data.service';
 })
 export class StoresComponent implements OnInit {
 
+  storeForm: FormGroup;
+  
+  
   title: string ="FIND A STORE";
   stores: string[] = ['Select your city', 'Agartala','Agra','Ahemdabad'];
-
-  constructor(private storesDataService: StoresDataService) { }
+  
+  constructor(private storesDataService: StoresDataService, private fb: FormBuilder) { 
+    this.createForm();
+  }
+  
+  createForm() {
+    this.storeForm = this.fb.group({selectCity: String});
+  }
 
   ngOnInit() {
     console.log("in store component");
     console.log(this.storesDataService.getStoresList());
+    console.log(this.storeForm.value + this.storeForm.status);
   }
   
   findStore(city: string) {
     this.storesDataService.getStoresList();
+    console.log(this.storeForm.get('selectCity').value + this.storeForm.status);
   }
 
 }
